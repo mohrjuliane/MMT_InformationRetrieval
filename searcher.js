c9b102577adfb0200f31922bfc8090e1293b8d29
Georg M. Sorst <g.sorst@findologic.com>
2020-04-13 12:53:41 +0200

    Add helpful hint.

--- searcher.js
+++ searcher.js
@@ -1,6 +1,7 @@
 export default class Searcher {
     constructor(files) {
         // TODO: Build the Inverted Index.
+        // Note that using a regular Object for the index might cause problems with terms such as "constructor".
     }
 
     search(query) {
