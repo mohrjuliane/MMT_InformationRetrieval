45d5013fbbdb1ccb1a77891226a32dad67950b2c
Juliane Mohr <julianemohr@hotmail.com>
2022-04-19 09:04:01 +0200

    Add implementation of finding relevantSets

--- searcher.js
+++ searcher.js
@@ -1,130 +1,190 @@
 export default class Searcher {
-    constructor(files) {
-        var fs = require("fs");
-        this.stopwords = fs.readFileSync("stop-words.txt", "utf-8").split("\n");
-        this.dictionary = this.createDictionary(files);
-    }
+                   constructor(files) {
+                       var fs = require("fs");
+                       this.num_docs = files.length;
+                       this.stopwords = fs.readFileSync("stop-words.txt", "utf-8").split("\n");
+                       this.dictionary = this.createDictionary(files);
+                   }
 
-    search(query) {
-        // let wordsList = this.preprocessingSearchTerm(query);
-        // let relevantSets = this.searchRelevantSets(wordsList);
+                   search(query) {
+                       let wordsList = this.preprocessingSearchTerm(query);
+                       let relevantSets = this.searchRelevantSets(wordsList);
 
-        // if (relevantSets.length != wordsList.length) {
-        //     return [];
-        // }
-        
-        // return this.compareSets(relevantSets);
-        return this.dictionary
-    }
+                       // if (relevantSets.length != wordsList.length) {
+                       //     return [];
+                       // }
 
-    compareSets(sets) {
-        while (sets.length > 1) {
-            let nextToLastSet = sets[0];
-            let lastSet = sets[1];
-            let intersection = this.intersection(nextToLastSet, lastSet);
+                       // return this.compareSets(relevantSets);
+                       
+                       return relevantSets;
+                   }
 
-            if (sets.length > 2) {
-                sets.shift(); //shift: delete first element of array
-                sets.shift();
-                sets.unshift(intersection); //unshift: add intersection at the beginning of array
-            } else {
-                sets = [intersection];
-            }
-        }
+                   compareSets(sets) {
+                       while (sets.length > 1) {
+                           let nextToLastSet = sets[0];
+                           let lastSet = sets[1];
+                           let intersection = this.intersection(nextToLastSet, lastSet);
 
-        return Array.from(sets[0]);
-    }
+                           if (sets.length > 2) {
+                               sets.shift(); //shift: delete first element of array
+                               sets.shift();
+                               sets.unshift(intersection); //unshift: add intersection at the beginning of array
+                           } else {
+                               sets = [intersection];
+                           }
+                       }
 
-    searchRelevantSets(wordsList) {
-        let relevantSets = [];
-        wordsList.forEach(word => {
-            if (this.dictionary.has(word)) {
-                relevantSets.push(this.dictionary.get(word));
-            } else {
-                return [];
-            }
-        });
-        return relevantSets;
-    }
+                       return Array.from(sets[0]);
+                   }
 
-    createDictionary(files) {
-        var fs = require("fs");
-        let vocabulary = new Map();
+                   searchRelevantSets(wordsList) {
+                       let relevantSets = []
+                       wordsList.forEach(word => {
+                           if (this.dictionary.has(word)) {
+                               let entry = this.dictionary.get(word);
+                               entry.sets.forEach(set => {
+                                   let foundSet = this.findSetOfFile(set.filename, relevantSets);
+                                   let score = entry.idf * set.frequency;
+                                   if (foundSet != undefined) {
+                                       //set exists already in relevantSets
+                                       foundSet.score += score;
+                                       foundSet.terms[word] = score; // set new score of word
+                                   } else {
+                                       let obj = {};
+                                       obj[word] = score;
+                                       relevantSets.push({
+                                           filename: set.filename,
+                                           score: score,
+                                           terms: obj
+                                       });
+                                   }
+                               });
+                           } else {
+                               return [];
+                           }
+                       });
+                       console.log(relevantSets)
+                       return relevantSets;
+                   }
 
-        for (const file of files) {
-            var text = fs.readFileSync(file, "utf-8");
-            let newText = this.preprocessingDictionary(text);
+                   findSetOfFile(filename, sets) {
+                       let found = undefined
+                        sets.forEach((set) => {
+                            console.log(set.filename, "set.filename")
+                            console.log(filename, "filename");
+                            if(set.filename === filename) {
+                                console.log("found!!!", set)
+                                found = set
+                                return set;
+                            }
+                        })
+                        return found
+                   }
 
-            //insert into dictionary
-            newText.forEach(function(word) {
-                if (vocabulary.has(word)) {
-                    let values = vocabulary.get(word);
-                    let foundEntry;
+                   createDictionary(files) {
+                       var fs = require("fs");
+                       let vocabulary = new Map();
 
-                    for(const entry of values) {
-                        if(entry.fileName === file) {
-                            foundEntry = entry
-                            break
-                        }
-                    }
+                       for (const file of files) {
+                           var text = fs.readFileSync(file, "utf-8");
+                           let newText = this.preprocessingDictionary(text);
 
-                    if(foundEntry != undefined) {
-                        foundEntry.frequency++
-                    } else { //word exists but its the first time it appears in this file
-                        vocabulary.set(
-                            word,
-                            values.add({
-                                fileName: file,
-                                frequency: 1
-                            })
-                        );
-                    }
-                } else { //word has not yet appeared in any file
-                    let set = new Set([{
-                        fileName: file,
-                        frequency: 1}]);
-                    vocabulary.set(word, set);
-                }
-            });
-        }
-        return vocabulary;
-    }
+                           //insert into dictionary
+                           newText.forEach(function(word) {
+                               if (vocabulary.has(word)) {
+                                   let values = vocabulary.get(word).sets; //sets of object
+                                   let foundEntry;
 
-    intersection(set1, set2) {
-        let intersectionArray = new Set();
-        for (let element of set2) {
-            if (set1.has(element)) {
-                intersectionArray.add(element);
-            }
-        }
-        return intersectionArray;
-    }
+                                   values.forEach(currentSet => {
+                                       if (currentSet.filename === file) {
+                                           foundEntry = currentSet;
+                                       }
+                                   });
 
-    preprocessingDictionary(words) {
-        let result = words
-            .replace(/[.,\/#!$%\^&\*;:{}=_`~()\n]/g, "")
-            .toLowerCase()
-            .split(" ")
-            .filter(element => !this.stopwords.includes(element));
-        return this.addHyphenWord(result);
-    }
+                                   if (foundEntry != undefined) {
+                                       foundEntry.frequency++;
+                                   } else {
+                                       //word exists but its the first time it appears in this file
+                                       // vocabulary.set(
+                                       //     word,
+                                       //     values.add({
+                                       //         filename: file,
+                                       //         frequency: 1
+                                       //     })
+                                       // );
+                                       values.add({
+                                           filename: file,
+                                           frequency: 1
+                                       });
+                                   }
+                               } else {
+                                   //word has not yet appeared in any file
+                                   let set = new Set([
+                                       {
+                                           filename: file,
+                                           frequency: 1
+                                       }
+                                   ]);
+                                   vocabulary.set(word, { idf: 0, sets: set });
+                               }
+                           });
+                       }
+                       vocabulary = this.setIDF(vocabulary);
+                       return vocabulary;
+                   }
 
-    preprocessingSearchTerm(words) {
-        let result = words
-            .toLowerCase()
-            .split(" ")
-            .filter(element => !this.stopwords.includes(element));
-        return result;
-    }
+                   setIDF(vocabulary) {
+                       vocabulary.forEach(entry => {
+                           let document_frequency = entry.sets.size;
+                           entry.idf = this.num_docs / document_frequency;
+                       });
+                       return vocabulary
+                    //    vocabulary.forEach((element, key) => {
+                    //        console.log(key + " => " + element.idf);
+                    //        element.sets.forEach(set => {
+                    //            console.log(set);
+                    //        });
+                    //        console.log("--------------");
+                    //    });
+                       //idf: num_total_documents/document_frequency
+                       //{idf: number, sets: Set{frequency: number, filename: string}, Set{frequency: number, filename: string}, .... 
+                   }
 
-    addHyphenWord(wordsList) {
-        wordsList.forEach(element => {
-            if (element.includes("-")) {
-                let hyphen = element.indexOf("-");
-                wordsList.push(element.substr(0, hyphen));
-                wordsList.push(element.substr(hyphen + 1, element.length - 1));
-            }
-        });
-        return wordsList;
-    }
-}
+                   intersection(set1, set2) {
+                       let intersectionArray = new Set();
+                       for (let element of set2) {
+                           if (set1.has(element)) {
+                               intersectionArray.add(element);
+                           }
+                       }
+                       return intersectionArray;
+                   }
+
+                   preprocessingDictionary(words) {
+                       let result = words
+                           .replace(/[.,\/#!$%\^&\*;:{}=_`~()\n]/g, "")
+                           .toLowerCase()
+                           .split(" ")
+                           .filter(element => !this.stopwords.includes(element));
+                       return this.addHyphenWord(result);
+                   }
+
+                   preprocessingSearchTerm(words) {
+                       let result = words
+                           .toLowerCase()
+                           .split(" ")
+                           .filter(element => !this.stopwords.includes(element));
+                       return result;
+                   }
+
+                   addHyphenWord(wordsList) {
+                       wordsList.forEach(element => {
+                           if (element.includes("-")) {
+                               let hyphen = element.indexOf("-");
+                               wordsList.push(element.substr(0, hyphen));
+                               wordsList.push(element.substr(hyphen + 1, element.length - 1));
+                           }
+                       });
+                       return wordsList;
+                   }
+               }
