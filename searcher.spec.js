f0a69fae31d9a7e3e67dab2c0adc53d8aab19bc4
Juliane Mohr <julianemohr@hotmail.com>
2022-04-20 09:47:51 +0200

    Fix pipeline normalized values

--- searcher.spec.js
+++ searcher.spec.js
@@ -22,27 +22,47 @@ describe("Searcher", () => {
             {
                 filename: "Archbishopric_of_Salzburg.txt",
                 score: 39614.142857142855,
-                terms: { salzburg: 39614.142857142855 }
+                scoreNormalized: 1.8014415266482002,
+                terms: { salzburg: 39614.142857142855 },
+                termsNormalized: {
+                    salzburg: 1.8014415266482002
+                }
             },
             {
                 filename: "Herbert_von_Karajan.txt",
                 score: 10803.857142857143,
-                terms: { salzburg: 10803.857142857143 }
+                scoreNormalized: 1.122614465443918,
+                terms: { salzburg: 10803.857142857143 },
+                termsNormalized: {
+                    salzburg: 1.122614465443918
+                }
             },
             {
                 filename: "Wolfgang_Amadeus_Mozart.txt",
                 score: 10803.857142857143,
-                terms: { salzburg: 10803.857142857143 }
+                scoreNormalized: 0.5800723313098094,
+                terms: { salzburg: 10803.857142857143 },
+                termsNormalized: {
+                    salzburg: 0.5800723313098094
+                }
             },
             {
                 filename: "Alfons_Schuhbeck.txt",
                 score: 3601.285714285714,
-                terms: { salzburg: 3601.285714285714 }
+                scoreNormalized: 0.7012679161079234,
+                terms: { salzburg: 3601.285714285714 },
+                termsNormalized: {
+                    salzburg: 0.7012679161079234
+                }
             },
             {
                 filename: "Carl_Maria_von_Weber.txt",
                 score: 3601.285714285714,
-                terms: { salzburg: 3601.285714285714 }
+                scoreNormalized: 0.40663969797233157,
+                terms: { salzburg: 3601.285714285714 },
+                termsNormalized: {
+                    salzburg: 0.40663969797233157
+                }
             }
         ]);
     });
@@ -53,23 +73,38 @@ describe("Searcher", () => {
             {
                 filename: "Anschluss.txt",
                 score: 3903.9037273452636,
+                scoreNormalized: 2.4820420929039946,
                 terms: {
                     austria: 3741.2650176678444,
                     germany: 162.63870967741937
+                },
+                termsNormalized: {
+                    austria: 1.7398420249580269,
+                    germany: 0.7422000679459677
                 }
             },
             {
                 filename: "Austria.txt",
                 score: 3806.320501538812,
+                scoreNormalized: 2.893861938428942,
                 terms: {
                     austria: 3741.2650176678444,
                     germany: 65.05548387096775
+                },
+                termsNormalized: {
+                    austria: 2.2380703881136843,
+                    germany: 0.6557915503152576
                 }
             },
             {
                 filename: "Germany.txt",
                 score: 2696.814166191725,
+                scoreNormalized: 2.5381179394581324,
                 terms: {
+                    austria: 712.6219081272085,
+                    germany: 1984.1922580645164
+                },
+                termsNormalized: {
                     austria: 712.6219081272085,
                     germany: 1984.1922580645164
                 }
@@ -77,17 +112,27 @@ describe("Searcher", () => {
             {
                 filename: "Treaty_of_Versailles.txt",
                 score: 2659.8038025760857,
+                scoreNormalized: 2.964782574159865,
                 terms: {
                     austria: 1781.5547703180212,
                     germany: 878.2490322580646
+                },
+                termsNormalized: {
+                    austria: 1.1174465267921516,
+                    germany: 1.8473360473677136
                 }
             },
             {
                 filename: "Prussia.txt",
                 score: 1865.1154359968086,
+                scoreNormalized: 2.225994093824934,
                 terms: {
                     austria: 1247.0883392226149,
                     germany: 618.0270967741936
+                },
+                termsNormalized: {
+                    austria: 0.7719953772142345,
+                    germany: 1.4539987166106996
                 }
             }
         ]);
@@ -98,15 +143,23 @@ describe("Searcher", () => {
             {
                 filename: "RNA.txt",
                 score: 25209,
+                scoreNormalized: 0.659569001234392,
                 terms: {
                     blueprint: 25209
+                },
+                termsNormalized: {
+                    blueprint: 0.659569001234392
                 }
             },
             {
                 filename: "_03_Bonnie_Clyde.txt",
                 score: 25209,
+                scoreNormalized: 0.5464929585960481,
                 terms: {
                     blueprint: 25209
+                },
+                termsNormalized: {
+                    blueprint: 0.5464929585960481
                 }
             }
         ]);
